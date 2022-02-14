fb246962e27a0af02230cf57f16793e70881a2b9
Marek Pękala <marekbf3@gmail.com>
2022-02-14 13:33:35 +0100

    Add HCFaceDetection stabilization
    * added stablization and comments for future improvements
    * reduced collection allocation, further perf improvements can address lambda allocations

--- WorkTimeAlghorithm/src/Detection/HcFaceDetection.cs
+++ WorkTimeAlghorithm/src/Detection/HcFaceDetection.cs
@@ -2,6 +2,9 @@
 using OpenCvSharp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
 
 namespace WMAlghorithm
 {
@@ -23,7 +26,9 @@ namespace WMAlghorithm
         private readonly CascadeClassifier _frontClassifier;
         private readonly CascadeClassifier _profileClassifier;
 
-        private Rect? _prevFirst;
+        private readonly Lazy<Rect[]> _singleElementArray =
+            new Lazy<Rect[]>(() => new Rect[1], LazyThreadSafetyMode.None);
+        private Rect? _previousFaceRect;
 
         public HcFaceDetection(IConfigurationService config)
         {
@@ -32,77 +37,120 @@ namespace WMAlghorithm
             _profileClassifier = new CascadeClassifier(settings.ProfileModel);
         }
 
-        private Rect[] RemoveOverlapping(Rect[] rects)
+        private IEnumerable<Rect> EnumerateRemoveNotOverlappingWithBiggest(IEnumerable<Rect> rects)
         {
-            if (rects.Length == 0)
+            var biggest = GetBiggest(rects);
+            foreach(var rect in rects)
+            {
+                if (biggest.IntersectsWith(rect))
+                {
+                    yield return rect;
+                }
+            }
+        }
+
+        private IEnumerable<Rect> RemoveNotOverlappingWithBiggest(IEnumerable<Rect> rects)
+        {
+            return !rects.Any() ? rects : EnumerateRemoveNotOverlappingWithBiggest(rects);
+        }
+
+        private Rect? Stabilize(in Rect biggest)
+        {
+            if (!_previousFaceRect.HasValue)
+            {
+                return null;
+            }
+
+            var yOverlap = Math.Min(_previousFaceRect.Value.Y + _previousFaceRect.Value.Height, biggest.Y + biggest.Height)
+                           - Math.Max(_previousFaceRect.Value.Y, biggest.Y);
+            var xOverlap = Math.Min(_previousFaceRect.Value.X + _previousFaceRect.Value.Width, biggest.X + biggest.Width)
+                           - Math.Max(_previousFaceRect.Value.X, biggest.X);
+
+            var prevArea = _previousFaceRect.Value.Height * _previousFaceRect.Value.Width;
+            var overlapArea = xOverlap * yOverlap;
+            var ratio = overlapArea / (float)prevArea;
+            if (!(ratio < 0.95f || ratio > 1f))
+            {
+                return _previousFaceRect.Value;
+            }
+            // face rect is moving - we can calculate score regarding rect move and when face is not detected for
+            // a few consecutive frames then we can assume that previous rect is a valid rect
+            return biggest;
+        }
+
+        private Rect GetBiggest(IEnumerable<Rect> rects)
+        {
+            return rects.OrderByDescending(r => r.Width * r.Height).First();
+        }
+
+        private IEnumerable<Rect> RemoveRectanglesDistantFromCenter(IEnumerable<Rect> rects, Mat frame)
+        {
+            var centerPoint = (x: frame.Width / 2, y: frame.Height / 2);
+            return rects.Where(r =>
+                centerPoint.x - r.X > r.Width * 2 == false &&
+                r.X + r.Width - centerPoint.x > r.Width * 2 == false);
+        }
+
+        private IEnumerable<Rect> RemoveDistantFromPrevious(IEnumerable<Rect> rects)
+        {
+            double Euclidean(in Rect r1, in Rect r2)
+            {
+                return Math.Sqrt(Math.Pow((r1.X + r1.Width/2) - (r2.X + r2.Width/2), 2) + Math.Pow((r1.Y + r1.Height/2) - (r2.Y + r2.Height/2), 2));
+            }
+
+            if (!_previousFaceRect.HasValue)
             {
                 return rects;
             }
 
-            var newRects = new List<Rect>();
-            var first = rects[0];
-            newRects.Add(first);
-            for (int i = 1; i < rects.Length; i++)
-            {
-
-                if (!first.IntersectsWith(rects[i]))
-                {
-                    newRects.Add(rects[i]);
-                }
-            }
-
-
-            return newRects.ToArray();
+            return rects.Where(r =>
+                Euclidean(r, _previousFaceRect.Value) <= _previousFaceRect.Value.Width/2d);
         }
 
-        private void Stabilize(Rect[] rects)
+        private Rect[] GetSingleElementArray(in Rect rect)
         {
-            if (!_prevFirst.HasValue)
+            _singleElementArray.Value[0] = rect;
+            return _singleElementArray.Value;
+        }
+
+        private Rect[] FilterFaceRects(Rect[] faceRects, Mat frame)
+        {
+            var faceRectsEnumerable = RemoveRectanglesDistantFromCenter(faceRects, frame);
+            faceRectsEnumerable = RemoveNotOverlappingWithBiggest(faceRectsEnumerable);
+            faceRectsEnumerable = RemoveDistantFromPrevious(faceRectsEnumerable);
+            if (!faceRectsEnumerable.Any())
             {
-                return;
+                // remove previous to prevent 'sticking' to biggest
+                _previousFaceRect = null;
+                return Array.Empty<Rect>();
             }
 
-            // if (_prevFirst.Value.IntersectsWith(rects[0]))
-            // {
-            //     rects[0] = _prevFirst.Value;
-            // }
-            //todo settings param 10, percent of scr
-            if (Math.Abs(_prevFirst.Value.X - rects[0].X) < 10 && Math.Abs(_prevFirst.Value.Y - rects[0].Y) < 10)
+            // get current biggest
+            var biggest = GetBiggest(faceRectsEnumerable);
+            // try to stabilize biggest rect with previously saved rect
+            var stabilized = Stabilize(biggest);
+            if (stabilized.HasValue)
             {
-                rects[0] = _prevFirst.Value;
+                _previousFaceRect = stabilized.Value;
+                return GetSingleElementArray(stabilized.Value);
             }
-        } 
+
+            // otherwise set previous to biggest
+            _previousFaceRect = biggest;
+            
+            return faceRectsEnumerable.ToArray();
+        }
 
         public Rect[] DetectFrontalFaces(Mat frame)
         {
             var faceRects = _frontClassifier.DetectMultiScale(frame);
-            faceRects = RemoveOverlapping(faceRects);
-            if (faceRects.Length > 0)
-            {
-                Stabilize(faceRects);
-                _prevFirst = faceRects[0];
-            }
-            else
-            {
-                _prevFirst = null;
-            }
-            return faceRects;
+            return FilterFaceRects(faceRects, frame);
         }
 
         public Rect[] DetectProfileFaces(Mat frame)
         {
             var faceRects = _profileClassifier.DetectMultiScale(frame);
-            faceRects = RemoveOverlapping(faceRects);
-            if (faceRects.Length > 0)
-            {
-                Stabilize(faceRects);
-                _prevFirst = faceRects[0];
-            }
-            else
-            {
-                _prevFirst = null;
-            }
-            return faceRects;
+            return FilterFaceRects(faceRects, frame);
         }
 
 
